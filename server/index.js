99c0dbb246354c2715294e5d6c760c81f82957c6
Mitch Henline <rmhenline@gmail.com>
2022-10-18 15:52:23 -0600

    done and working

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const express = require("express")
+const cors = require("cors")
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+
+const {getHouses, deleteHouse, createHouse, updateHouse} = require('./controller.js')
+
+//endpoints
+app.get('/api/houses', getHouses)
+app.post('/api/houses', createHouse)
+app.put('/api/houses/:id', updateHouse)
+app.delete('/api/houses/:id', deleteHouse)
+
+
+const PORT = 4004
+app.listen(PORT, () => console.log(`Server is running on ${PORT}`))
